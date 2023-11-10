5f998cc6b6eadece95a4a3bc75457ba962a434e6
rvincoy <vin23002@byui.edu>
2023-11-10 10:01:25 -0600

    fraction wk3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3,4);        
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetTop());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        f1.SetTop(1);
+        f1.SetBottom(3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }
