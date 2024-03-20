8b89bef71868cc20068c99bad465f37d3a1e50a9
rvincoy <vin23002@byui.edu>
2024-03-19 19:42:48 -0500

    cs212

---
+++ cs212/mystack.py
@@ -0,0 +1,32 @@
+def convert2String(stack):
+#### Add your code to convert the list into a string 
+    pass
+
+#### Do not change this part
+stack = []
+stack.append('s')
+stack.append('g')
+stack.append('n')
+stack.append('i')
+stack.append('R')
+stack.append(' ')
+value=stack.pop()
+stack.append(value)
+stack.append('e')
+stack.append('h')
+stack.append('t')
+stack.append(value)
+stack.append('f')
+stack.append('o')
+stack.append(value)
+value=stack.pop()
+stack.append(' ')
+stack.append('d')
+stack.append('r')
+stack.append('o')
+stack.append('L')
+stack.append(' ')
+stack.append('e')
+stack.append('h')
+stack.append('T')
+print(convert2String(stack)) ### The Lord of the rings
---
+++ cs212/mystack_Solved.py
@@ -0,0 +1,35 @@
+def convert2String(stack):
+#### Add your code to convert the list into a string 
+    str=""
+    while len(stack)>0:
+        str = str + stack.pop()
+    return str
+
+#### Do not change this part
+stack = []
+stack.append('s')
+stack.append('g')
+stack.append('n')
+stack.append('i')
+stack.append('R')
+stack.append(' ')
+value=stack.pop()
+stack.append(value)
+stack.append('e')
+stack.append('h')
+stack.append('t')
+stack.append(value)
+stack.append('f')
+stack.append('o')
+stack.append(value)
+value=stack.pop()
+stack.append(' ')
+stack.append('d')
+stack.append('r')
+stack.append('o')
+stack.append('L')
+stack.append(' ')
+stack.append('e')
+stack.append('h')
+stack.append('T')
+print(convert2String(stack)) ### The Lord of the rings
